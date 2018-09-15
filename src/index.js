7522cdf80046f94cafbd826d1241ee030f613fd2
Allison12345 <2562392373@qq.com>
2018-09-15 17:05:05 +0800

    clock.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import Comment from './comment-1'
+import Clock from './clock'
 
-ReactDOM.render(<Comment
+ReactDOM.render(<Clock
     />, document.getElementById('root'));
 registerServiceWorker();
